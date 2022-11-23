e3b6e7da70701b5cf145f1e368e6fc9897118cce
Youssef Helwa <yhelwa12@gmail.com>
2022-11-23 22:15:16 +0200

    Modified Coffee Size

--- lib/pages/coffee_size.dart
+++ lib/pages/coffee_size.dart
@@ -25,26 +25,28 @@ class _CoffeeSizeState extends State<CoffeeSize> {
         itemCount: coffeeSize.length,
         itemBuilder: (context, index) {
           String coffeeKey = coffeeSize.keys.elementAt(index);
-          return Container(
-            width: 110,
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-                border: Border.all(
+          return InkWell(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            onTap: () {
+              setState(() {
+                coffeeSize.updateAll((key, value) => value = false);
+                coffeeSize[coffeeKey] = true;
+              });
+            },
+            child: Container(
+              width: 110,
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: coffeeSize[coffeeKey]!
+                        ? Colors.orange.shade700
+                        : Colors.transparent,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
                   color: coffeeSize[coffeeKey]!
-                      ? Colors.orange.shade700
-                      : Colors.transparent,
-                ),
-                borderRadius: BorderRadius.circular(15),
-                color: coffeeSize[coffeeKey]!
-                    ? const Color.fromRGBO(14, 17, 21, 1)
-                    : const Color.fromRGBO(19, 25, 34, 1)),
-            child: InkWell(
-              onTap: () {
-                setState(() {
-                  coffeeSize.updateAll((key, value) => value = false);
-                  coffeeSize[coffeeKey] = true;
-                });
-              },
+                      ? const Color.fromRGBO(14, 17, 21, 1)
+                      : const Color.fromRGBO(19, 25, 34, 1)),
               child: Center(
                 child: Text(
                   coffeeKey,
